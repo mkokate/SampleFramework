49948b216f2fc2e23f352c1afedcaae9b50f913b
mkokate <mahesh@operamediaworks.com>
2018-11-17 15:03:31 +0530

    Added missed files

---
+++ SampleFramework/RandomColor.swift
@@ -0,0 +1,14 @@
+//
+//  RandomColor.swift
+//  SampleFramework
+//
+//  Created by Mahesh Kokate on 17/11/18.
+//  Copyright © 2018 Mahesh Kokate. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public func randomColor() -> UIColor {
+    return UIColor.red
+}
---
+++ SampleFramework/SampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  SampleFramework.h
+//  SampleFramework
+//
+//  Created by Mahesh Kokate on 17/11/18.
+//  Copyright © 2018 Mahesh Kokate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleFramework.
+FOUNDATION_EXPORT double SampleFrameworkVersionNumber;
+
+//! Project version string for SampleFramework.
+FOUNDATION_EXPORT const unsigned char SampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFramework/PublicHeader.h>
+
+
